66cc788884ae8d9ebe334afc0a2d1ee507fdac64
Ching Chen <chingyg407@gmail.com>
2024-05-08 13:32:54 -0400

    components and databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,24 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
       'this is simply a test',
       'https://easyweeknightrecipes.com/wp-content/uploads/2021/06/meatloaf-recipe-4-1140x1710.jpg'
     ),
+    new Recipe(
+      'Another Test Recipe',
+      'this is simply a test',
+      'https://easyweeknightrecipes.com/wp-content/uploads/2021/06/meatloaf-recipe-4-1140x1710.jpg'
+    ),
   ];
   ngOnInit(): void {
     throw new Error('Method not implemented.');
+  }
+
+  onRecipeSelected(currentRecipe: Recipe) {
+    this.recipeWasSelected.emit(currentRecipe);
   }
 }
